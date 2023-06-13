6ab6561ca94a8f5e48efeb8933e107fbcc449ba0
Danijel Domazet <danijel.domazet@littleendian.com>
2023-06-13 13:38:09 +0000

    Working but CUDA out of mem.

--- make_ec2work.sh
+++ make_ec2work.sh
@@ -4,5 +4,5 @@ path=egs/ec2work/train
 if [[ ! -e $path ]]; then
     mkdir -p $path
 fi
-python3 -m denoiser.audio /home/ubuntu/datasets/valentini/noisy > $path/noisy.json
-python3 -m denoiser.audio /home/ubuntu/datasets/valentini/clean > $path/clean.json
+python3 -m denoiser.audio /home/ubuntu/datasets/valentini/noisy_trainset_28spk_wav > $path/noisy.json
+python3 -m denoiser.audio /home/ubuntu/datasets/valentini/clean_trainset_28spk_wav > $path/clean.json
